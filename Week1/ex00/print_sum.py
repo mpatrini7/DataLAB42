2882a3d7dcc60f9912fc2042c93589a84e7bb6d0
mpatrini7 <m.patrini90@gmail.com>
2022-05-18 03:53:55 +0200

    Week1

--- Week1/ex00/print_sum.py
+++ Week1/ex00/print_sum.py
@@ -6,16 +6,25 @@
 #    By: mpatrini <mpatrini@student.42roma.it>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/16 15:39:49 by mpatrini          #+#    #+#              #
-#    Updated: 2022/05/16 15:39:51 by mpatrini         ###   ########.fr        #
+#    Updated: 2022/05/18 03:51:00 by mpatrini         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
 if len(sys.argv) == 3:
-	if sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
-		print(int(sys.argv[1]) + int(sys.argv[2]))
-	else:
-		print("Numbers only!")
+	try:
+		a = float(sys.argv[1])
+		b = float(sys.argv[2])
+		c = a + b
+		if c.is_integer():
+			print(int(c))
+		else:
+			print("{:.2f}".format(a + b))		
+	except ValueError:
+		if sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
+			print(int(sys.argv[1]) + int(sys.argv[2]))
+		else:
+			print("Numbers only!")
 else:
 	print("Wrong number of arguments!")
